3f296a09f2e8dff1c58cdf58471b4cb1888ee27a
alphamen01 <lesg.2018@hotmail.com>
2023-11-08 16:01:25 -0500

    se desarrollo las peticiones en el usuariocontroller para el modulo de usuario,se creo usuario_index.js especificamente para la vista usuario/index donde se implementara las peticiones del usuariocontroller

--- SistemaVentas.AplicacionWeb/Controllers/UsuarioController.cs
+++ SistemaVentas.AplicacionWeb/Controllers/UsuarioController.cs
@@ -1,12 +1,143 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using AutoMapper;
+using Newtonsoft.Json;
+using SistemaVentas.AplicacionWeb.Models.ViewModels;
+using SistemaVentas.AplicacionWeb.Utilidades.Response;
+using SistemaVentas.BLL.Interfaces;
+using SistemaVentas.Entity.Models;
+
 namespace SistemaVentas.AplicacionWeb.Controllers
 {
     public class UsuarioController : Controller
     {
+        private readonly IMapper _mapper;
+        private readonly IUsuarioService _usuarioService;
+        private readonly IRolService _rolService;
+
+        public UsuarioController(IMapper mapper, IUsuarioService usuarioService, IRolService rolService)
+        {
+            _mapper = mapper;
+            _usuarioService = usuarioService;
+            _rolService = rolService;
+
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListaRoles()
+        {
+            var lista = await _rolService.Lista();
+            List<VMRol> vmListaRoles = _mapper.Map<List<VMRol>>(lista);
+            return StatusCode(StatusCodes.Status200OK,vmListaRoles);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> ListaUsuarios()
+        {
+            var lista = await _usuarioService.Lista();
+            List<VMUsuario> vmListaUsuarios = _mapper.Map<List<VMUsuario>>(lista);
+            return StatusCode(StatusCodes.Status200OK, new { data = vmListaUsuarios }); //por el formato del datatable de jquery
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear([FromForm] IFormFile foto, [FromForm] string modelo)
+        {
+            GenericResponse<VMUsuario> gResponse = new GenericResponse<VMUsuario>();
+            try
+            {
+                VMUsuario vmUsuario = JsonConvert.DeserializeObject<VMUsuario>(modelo)!;
+
+                string nombreFoto = "";
+
+                Stream fotoStream = null;
+
+                if (foto != null)
+                {
+                    string nombre_en_codigo = Guid.NewGuid().ToString("N");
+                    string extension = Path.GetExtension(foto.FileName);
+                    nombreFoto = string.Concat(nombre_en_codigo, extension);
+                    fotoStream = foto.OpenReadStream();
+                }
+
+                string urlPlantillaCorreo = $"{this.Request.Scheme}://{this.Request.Host}/Plantilla/EnviarClave?correo=[correo]&clave=[clave]";
+
+                Usuario usuario_creado = await _usuarioService.Crear(_mapper.Map<Usuario>(vmUsuario), fotoStream!, nombreFoto, urlPlantillaCorreo);
+
+                vmUsuario = _mapper.Map<VMUsuario>(usuario_creado);
+
+                gResponse.Estado = true;
+                gResponse.Objeto = vmUsuario;
+
+            }
+            catch (Exception ex)
+            {
+                gResponse.Estado = false;
+                gResponse.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, gResponse);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> Editar([FromForm] IFormFile foto, [FromForm] string modelo)
+        {
+            GenericResponse<VMUsuario> gResponse = new GenericResponse<VMUsuario>();
+            try
+            {
+                VMUsuario vmUsuario = JsonConvert.DeserializeObject<VMUsuario>(modelo)!;
+
+                string nombreFoto = "";
+
+                Stream fotoStream = null;
+
+                if (foto != null)
+                {
+                    string nombre_en_codigo = Guid.NewGuid().ToString("N");
+                    string extension = Path.GetExtension(foto.FileName);
+                    nombreFoto = string.Concat(nombre_en_codigo, extension);
+                    fotoStream = foto.OpenReadStream();
+                }
+
+                //string urlPlantillaCorreo = $"{this.Request.Scheme}://{this.Request.Host}/Plantilla/EnviarClave?correo=[correo]&clave=[clave]";
+
+                Usuario usuario_editado = await _usuarioService.Editar(_mapper.Map<Usuario>(vmUsuario), fotoStream!, nombreFoto);
+
+                vmUsuario = _mapper.Map<VMUsuario>(usuario_editado);
+
+                gResponse.Estado = true;
+                gResponse.Objeto = vmUsuario;
+
+            }
+            catch (Exception ex)
+            {
+                gResponse.Estado = false;
+                gResponse.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, gResponse);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Eliminar(int idUsuario)
+        {
+            GenericResponse<string> gResponse = new GenericResponse<string>();
+
+            try
+            {
+                gResponse.Estado = await _usuarioService.Eliminar(idUsuario);
+            }
+            catch(Exception ex) 
+            {
+                gResponse.Estado = false;
+                gResponse.Mensaje = ex.Message;
+
+            }
+
+            return StatusCode(StatusCodes.Status200OK, gResponse);
         }
     }
 }
